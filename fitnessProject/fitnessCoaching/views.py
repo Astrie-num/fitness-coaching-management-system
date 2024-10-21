4b2d47ff3c475dbdac9fae4d81abe8946403f0dc
MONKEY <astrie@MONKEY>
2024-10-21 20:56:16 +0200

    Added views

--- fitnessProject/fitnessCoaching/views.py
+++ fitnessProject/fitnessCoaching/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Coaches
+
+def display_coaches(request):
+    coaches = Coaches.objects.all()
+    return render(request,'coaches.html', {'coaches:' coaches})
--- fitnessProject/fitnessProject/settings.py
+++ fitnessProject/fitnessProject/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = "fitnessProject.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(fitnessCoaching),'templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
